c50885862528365634c2d86efdfa17ae7b0440fe
Jack T <jackmt9@gmail.com>
2020-01-13 15:59:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,17 @@ end
 
 
 def full?(board)
-board.all? {|index| index == "X" || index == "O"}
+  counter = 0
+  board.each do |possible_Token|
+    if possible_Token == "O" || possible_Token == "X"
+      counter+=1
+    end
+  end
+  if counter == 9
+    return true
+  else
+    return false
+  end
 end
 
 def draw?(board)
